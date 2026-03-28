3f534ea72437257fb9e5ab65b6b333458c4a7dc0
Carolina-GSantiago <CarolinaGorrizSantia@cmail.carleton.ca>
2026-03-28 17:32:43 -0400

    finished button styles for events, added back to top button

--- js/events.js
+++ js/events.js
@@ -376,3 +376,18 @@ const button = document.getElementById('viewSelect');
 button.addEventListener('click', function() {
   this.classList.add('viewSelectClicked');
 });
+
+
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox and Opera
+}
